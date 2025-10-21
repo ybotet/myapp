2c0e7e23b86aa3f8b6f1597ac9934df4fcb3f248
Yaisel Botet Cesar <72634632+ybotet@users.noreply.github.com>
2025-10-21 18:48:24 +0300

    Update app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,8 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/ybotet/myapp/internal/app/handlers"
 	"github.com/ybotet/myapp/utils"
@@ -35,17 +33,6 @@ func Run() {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprintln(w, "Hello, Go project structure!")
 	})
-
-	// // Пример JSON-ручки: /ping
-	// mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-	// 	utils.LogRequest(r)
-	// 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	// 	_ = json.NewEncoder(w).Encode(pingResp{
-	// 		Status: "ok",
-	// 		Time:   time.Now().UTC().Format(time.RFC3339),
-	// 	})
-	// })
-
 	mux.HandleFunc("/fail", func(w http.ResponseWriter, r *http.Request) {
 		utils.LogRequest(r)
 		utils.WriteErr(w, http.StatusBadRequest, "bad_request_example")
@@ -55,8 +42,8 @@ func Run() {
 
 	mux.HandleFunc("/ping", handlers.Ping)
 
-	utils.LogInfo("Server is starting on :8080")
-	if err := http.ListenAndServe(":8080", handler); err != nil {
+	utils.LogInfo("Server is starting on :8081")
+	if err := http.ListenAndServe(":8081", handler); err != nil {
 		utils.LogError("server error: " + err.Error())
 	}
 }
